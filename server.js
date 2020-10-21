61e8d1600a5c9488a24a886b8c41b62f930733c5
Tranquility <46429872+TranquilityTech@users.noreply.github.com>
2020-10-21 11:09:48 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ var io = sockio.listen(app.listen(post), {
     log: true
 });
 
+io.origins((origin, callback) => {
+    /*
+    if (origin !== 'https://foo.example.com') {
+        return callback('origin not allowed', false);
+    }
+    */
+    callback(null, true);
+});
+
 io.on('connection', async (socket) => {
     console.log('connection');
 });
