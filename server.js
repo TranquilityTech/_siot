27c88a36a00761629ef61329dcee03844d2c298a
Tranquility <46429872+TranquilityTech@users.noreply.github.com>
2020-10-21 11:01:32 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ var app = express();
 // enable cors for everyone
 app.use(cors());
 
-// serve static files from the "public" directory
-app.use(express.static('public'));
-
 // start the app on port specified
 var post = 3000;
 var io = sockio.listen(app.listen(post), {log: true});
